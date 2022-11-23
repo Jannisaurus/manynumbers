50c86d3452cf230c3e59c32bec0f8e7e51c52034
Jannisaurus <117642071+Jannisaurus@users.noreply.github.com>
2022-11-23 22:05:54 +0100

    Many functions!
    Added scanner. Loop for returning the random number. Keeps score.

--- src/manynumbers/Main.java
+++ src/manynumbers/Main.java
@@ -1,27 +1,41 @@
 package manynumbers;
 
-import java.util.Random; 
+//import java.util.Random; 
 import java.util.concurrent.ThreadLocalRandom; 
+import java.util.Scanner; 
 
 public class Main {
 public static void main(String[] args) {
 	
-	int min = 1000;
-	int max = 9999; 
+//	int min = 99;
+//	int max = 9999; 
 //	double a = Math.random()*(max-min+1) + min; 
 //	System.out.println(a);
+//
+//	int b = (int)(Math.random()*(max-min+1)+min);
+//	System.out.println(b);
+//	
+//	Random x = new Random(); 
+//	int z = x.nextInt(1000);
+//	System.out.println(z);
+	
 
-	int b = (int)(Math.random()*(max-min+1)+min);
-	System.out.println(b);
+//	int localRandom2 = ThreadLocalRandom.current().nextInt();
+	int score = 0; 
+	int tot = 0;
+	Scanner scanner = new Scanner(System.in);
+
 	
-	Random x = new Random(); 
-	int z = x.nextInt(1000);
-	System.out.println(z);
+	for(tot = 0; tot < 5; tot++) {	
+		int localRandom1 = ThreadLocalRandom.current().nextInt(999,10000);
+		System.out.println(localRandom1);
+		int userInt = scanner.nextInt();
+		if(userInt == localRandom1) {
+			score++; 
+		} 
+	}
+	System.out.println("You're score is: " + score); 
 	
-	int localRandom1 = ThreadLocalRandom.current().nextInt(99,10000);
-	int localRandom2 = ThreadLocalRandom.current().nextInt();
-	
-	System.out.println(localRandom1);
 	
 
 //	System.out.println(Math.random());
