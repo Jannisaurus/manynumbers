18127002601088a43ba8ce8135496fef3cdbcf8b
Jannisaurus <117642071+Jannisaurus@users.noreply.github.com>
2022-11-24 08:35:17 +0100

    Play command
    Added play command

--- src/manynumbers/Main.java
+++ src/manynumbers/Main.java
@@ -21,23 +21,34 @@ public static void main(String[] args) {
 	
 
 //	int localRandom2 = ThreadLocalRandom.current().nextInt();
+	
 	int score = 0; 
 	int tot = 0;
 	Scanner scanner = new Scanner(System.in);
 
+	String command; 
 	
-	for(tot = 0; tot < 5; tot++) {	
-		int localRandom1 = ThreadLocalRandom.current().nextInt(999,10000);
-		System.out.println(localRandom1);
-		int userInt = scanner.nextInt();
-		if(userInt == localRandom1) {
-			score++; 
-		} 
-	}
-	System.out.println("You're score is: " + score); 
-	
-	
+	while(true) {
 
+	System.out.println("Press play");
+	command = scanner.nextLine(); 
+	
+	if(command.equals("play")) {
+		for(tot = 0; tot < 2; tot++) {	
+			int localRandom1 = ThreadLocalRandom.current().nextInt(999,10000);
+			System.out.println(localRandom1);
+			int userInt = scanner.nextInt();
+			if(userInt == localRandom1) {
+				score++; 
+			} 
+		}
+		
+		System.out.println("You're score is: " + score); 
+		scanner.nextLine();
+	} 
+	
+	
 //	System.out.println(Math.random());
+	}
 }
 }
